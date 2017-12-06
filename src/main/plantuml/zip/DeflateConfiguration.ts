e94a7b9b2d8c1f0fad342d1556944f06a28b7149
kazuhito-m <kazuhito.sumpic@gmail.com>
2017-12-07 02:33:40 +0900

    リファクタリング。クラスのカプセル化促進。

--- src/main/plantuml/zip/DeflateConfiguration.ts
+++ src/main/plantuml/zip/DeflateConfiguration.ts
@@ -1,14 +1,31 @@
 export default class DeflateConfiguration {
-    public readonly max_chain: number; // reduce lazy search above this match length
-    public readonly nice_length: number; // do not perform lazy search above this match length
-    public readonly max_lazy: number; // quit search above this match length
-    public readonly good_length: number;
+
+    private readonly _maxChain: number; // reduce lazy search above this match length
+    private readonly _niceLength: number; // do not perform lazy search above this match length
+    private readonly _maxLazy: number; // quit search above this match length
+    private readonly _goodLength: number;
+
+    get max_chain(): number {
+        return this._maxChain;
+    }
+
+    get nice_length():number {
+        return this._niceLength;
+    }
+
+    get max_lazy():number {
+        return this._maxLazy;
+    }
+
+    get good_length():number {
+        return this._goodLength;
+    }
 
     constructor(a: number, b: number, c: number, d: number) {
-        this.good_length = a;
-        this.max_lazy = b;
-        this.nice_length = c;
-        this.max_chain = d;
+        this._goodLength = a;
+        this._maxLazy = b;
+        this._niceLength = c;
+        this._maxChain = d;
     }
 
 }
--- src/main/plantuml/zip/DeflateTreeDesc.ts
+++ src/main/plantuml/zip/DeflateTreeDesc.ts
@@ -1,6 +1,7 @@
 import DeflateCT from './DeflateCT';
 
 export default class DeflateTreeDesc {
+
     public dyn_tree: Array<DeflateCT>;	// the dynamic tree
     public static_tree: Array<DeflateCT> | null;	// corresponding static tree or NULL
     public extra_bits: Array<number>;	// extra bits for each code or NULL
@@ -8,4 +9,5 @@ export default class DeflateTreeDesc {
     public elems = 0;		// max number of elements in the tree
     public max_length = 0;	// max bit length for the codes
     public max_code = 0;		// largest code with non zero frequency
+
 }
