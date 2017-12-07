71218a1196000744b6616b7e6f14689c5c4b590c
kazuhito-m <kazuhito.sumpic@gmail.com>
2017-12-08 04:03:23 +0900

    実装。URL作成のロジックを実装。

--- src/main/plantuml/PlantUmlServerUrl.ts
+++ src/main/plantuml/PlantUmlServerUrl.ts
@@ -1,13 +1,29 @@
+import * as querystring from 'querystring';
+import * as iconv from 'iconv-lite';
+import OriginalZip from './zip/OriginalZip'
+import OriginalEncoder from './encode/OriginalEncoder'
+
 export default class PlantUmlServerUrl {
 
-    private readonly umlText:string;
+    private static readonly URL_HEAD = 'http://www.plantuml.com/plantuml/png/';
 
-    public constructor(umlText:string) {
+    private readonly umlText: string;
+
+    public constructor(umlText: string) {
         this.umlText = umlText;
     }
 
-    public generate():string {
-        return "";
+    public generate(): string {
+        const encoded = encodeURIComponent(this.umlText);
+        const decoded = querystring.unescape(encoded);
+
+        const charsetConverted = iconv.decode(Buffer.from(decoded, 'UTF-8'), 'ISO-8859-1');
+
+        const deflated = new OriginalZip().deflate(charsetConverted, 9);
+
+        const result = new OriginalEncoder().encode64(deflated);
+
+        return PlantUmlServerUrl.URL_HEAD + result;
     }
 
 }
