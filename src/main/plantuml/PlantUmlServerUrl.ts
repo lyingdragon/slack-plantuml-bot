e7f41b023e3e7933ee16bee4baa232a03448e8f7
kazuhito-m <kazuhito.sumpic@gmail.com>
2017-12-17 03:54:39 +0900

    実装。UMLの文字列を「できる限り認識するよう補正する」前処理をを入れる。

--- src/main/plantuml/PlantUmlServerUrl.ts
+++ src/main/plantuml/PlantUmlServerUrl.ts
@@ -2,6 +2,8 @@ import * as querystring from 'querystring';
 import * as iconv from 'iconv-lite';
 import OriginalZip from './zip/OriginalZip'
 import OriginalEncoder from './encode/OriginalEncoder'
+import { normalize } from 'path';
+import { triggerAsyncId } from 'async_hooks';
 
 export default class PlantUmlServerUrl {
 
@@ -14,7 +16,12 @@ export default class PlantUmlServerUrl {
     }
 
     public generate(): string {
-        const encoded = encodeURIComponent(this.umlText);
+        const normalizedText: string = this.normalize();
+        if (normalizedText.length === 0) return '';
+
+        const fixdUmlText = "@startuml\n" + normalizedText + "\n@enduml";
+
+        const encoded = encodeURIComponent(fixdUmlText);
         const decoded = querystring.unescape(encoded);
 
         const charsetConverted = iconv.decode(Buffer.from(decoded, 'UTF-8'), 'ISO-8859-1');
@@ -24,6 +31,17 @@ export default class PlantUmlServerUrl {
         const result = new OriginalEncoder().encode64(deflated);
 
         return PlantUmlServerUrl.URL_HEAD + result;
+    }
+
+    normalize(): string {
+        const trimed = this.umlText.trim();
+        if (trimed.length === 0) return '';
+        const crEscape = trimed.replace(/\n/g, '<cr>');
+        const frontCut = crEscape.replace(/^.*@startuml/i, '').trimLeft();
+        const endCut = frontCut.replace(/@enduml.*$/i, '').trimRight();
+        const crTrimed = endCut.replace(/^(<cr>)*/, '').replace(/(<cr>).*$/, '');
+        const crRecoverd = crTrimed.replace(/<cr>/g, "\n");
+        return crRecoverd.trim();
     }
 
 }
--- src/test/plantuml/PlantUmlServerUrlTest.ts
+++ src/test/plantuml/PlantUmlServerUrlTest.ts
@@ -1,4 +1,5 @@
 import PlantUmlServerUrl from '../../main/plantuml/PlantUmlServerUrl';
+import { access } from 'fs';
 
 describe('PlantUMLサイトのURLを作成するテスト', () => {
 
@@ -10,6 +11,56 @@ describe('PlantUMLサイトのURLを作成するテスト', () => {
 
             const expected = 'http://www.plantuml.com/plantuml/png/SoWkIImgAStDuKhEIImkLdWsVUcpEMjUh9h7vP2Qbm8K1m00';
             expect(actual).toContain(expected);
+        })
+    });
+
+    describe('nomarize()のテスト', () => {
+        test('両脇のUML宣言があった場合、削除する', () => {
+            const parameter: string = "@startuml\nclass 日本語\n@enduml";
+
+            const actual = new PlantUmlServerUrl(parameter).normalize();
+
+            expect(actual).toEqual('class 日本語');
+        })
+
+        test('前にUML宣言があり、その前にも文字列があった場合、削除する', () => {
+            const parameter: string = "前にある不必要と思しき文字列\n@startuml\nclass 日本語";
+
+            const actual = new PlantUmlServerUrl(parameter).normalize();
+
+            expect(actual).toEqual('class 日本語');
+        })
+
+        test('後ろにUML宣言があり、その後にも文字列があった場合、削除する', () => {
+            const parameter: string = "class 日本語@enduml\n後ろにある不必要と思しき文字列";
+
+            const actual = new PlantUmlServerUrl(parameter).normalize();
+
+            expect(actual).toEqual('class 日本語');
+        })
+
+        test('空白やタブで作られている文字列は、空文字に変換する', () => {
+            const parameter: string = " \t \t";
+
+            const actual = new PlantUmlServerUrl(parameter).normalize();
+
+            expect(actual).toEqual('');
+        })
+
+        test('空白と、前後に改行が連打されていたとしても、空文字に変換する', () => {
+            const parameter: string = "\n\n\n  \n\n\n";
+
+            const actual = new PlantUmlServerUrl(parameter).normalize();
+
+            expect(actual).toEqual('');
+        })
+
+        test('UML宣言に大文字小文字が混在していたとしても、削除する', () => {
+            const parameter: string = "前\n@StArTuMl\n\n\nclass Test\n\n@eNdUmL\n後";
+
+            const actual = new PlantUmlServerUrl(parameter).normalize();
+
+            expect(actual).toEqual("class Test");
         })
     });
 
